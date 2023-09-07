46e971446ce7301e46efc9dda8dcf6a114a996ae
NitishGhormade <118716634+NitishGhormade@users.noreply.github.com>
2023-09-07 13:06:13 +0530

    NumPy_and_MathLibrary.py

--- MathLibrary.py
+++ MathLibrary.py
@@ -1,5 +1,6 @@
 # Q3 Analyze math library and numpy library
 
+#math library
 import math
 print("Value of e: ",math.e)
 print("Value of Pi: ",math.pi)
@@ -12,6 +13,17 @@ print("Factorial of Number: ",math.factorial(num))
 
 num = int(input("Enter any number to find its Absolute: "))
 print("Absolute of Number: ",math.fabs(num))
+
+
+
+#numpy library
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
 
 
 
